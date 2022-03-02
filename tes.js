5ea7d35f482429a76ce1148504953a38be1a72bb
Syaiful_505 <65906055+syaiful505@users.noreply.github.com>
2022-03-03 02:33:03 +0700

    answer sur easy higher value js

---
+++ tes.js
@@ -0,0 +1,14 @@
+/**
+ * Direction:
+ * Find the higher value from the array bellow
+ *
+ * Expected Result:
+ * 8
+ */
+ let numbers = [3, 1, 2, 3, 7, 5, 6, 8, 2, 1];
+
+ function result(numbers) {
+    return Math.max.apply(null, numbers);
+ }
+ 
+ console.log(result(numbers));
